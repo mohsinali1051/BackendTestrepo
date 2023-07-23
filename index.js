715b3172f2e132019179f4235bf07c590b4dcbd7
ahmed <iamahmad9211@gmail.com>
2023-07-23 23:49:34 +0500

    main

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ dotenv.config();
 const port = process.env.PORT; // Use the PORT environment variable or default to 3000
 
 app.get("/",(req,res)=>{
-    res.send("Server is running")})
+   return res.send("Server is running")})
 // API 1 
 app.get('/api', (req, res) => {
-    res.status(200).json({ message: 'API 1 is working!' });
+    return res.status(200).json({ message: 'API 1 is working!' });
 });
 // API 2 
 app.get('/apitest', (req, res) => {
-    res.status(200).json({ message: 'API 2 is working!' });
+    return res.status(200).json({ message: 'API 2 is working!' });
 });
 
 // Start the server
